1aee83922f392bdabeb7693d69a7909437c733c5
Alfred Tchondjo <iriven@yahoo.fr>
2017-05-09 19:25:14 +0200

    Update InvalidOptionsException.php

--- Exceptions/InvalidOptionsException.php
+++ Exceptions/InvalidOptionsException.php
@@ -1,1 +1,19 @@
+<?php
 
+/**
+ * Created by PhpStorm.
+ * User: sjhc1170
+ * Date: 20/06/2016
+ * Time: 09:41
+ */
+
+namespace Iriven\Exceptions;
+/**
+ * Thrown when the value of an option does not match its validation rules.
+ *
+ * You should make sure a valid value is passed to the option.
+ *
+ */
+class InvalidOptionsException extends InvalidArgumentException
+{
+}
