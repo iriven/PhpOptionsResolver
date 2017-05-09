995a87972c806c8d6b857521bf5554ad9ef9ded2
Alfred Tchondjo <iriven@yahoo.fr>
2017-05-09 19:23:22 +0200

    Update AccessException.php

--- Exceptions/AccessException.php
+++ Exceptions/AccessException.php
@@ -1,1 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sjhc1170
+ * Date: 20/06/2016
+ * Time: 09:41
+ */
 
+namespace Iriven\Exceptions;
+use Iriven\Interfaces\ExceptionInterface;
+
+/**
+ * Thrown when trying to read an option outside of or write it inside of
+ * {@link \Iriven\PhpOptionsResolver::resolve()}.
+ *
+ */
+class AccessException extends \LogicException implements ExceptionInterface
+{
+
+}
