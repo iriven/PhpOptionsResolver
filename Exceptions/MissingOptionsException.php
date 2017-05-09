1c70e31b8225fc2728e647e808d295c13ea9a23e
Alfred Tchondjo <iriven@yahoo.fr>
2017-05-09 19:25:54 +0200

    Update MissingOptionsException.php

--- Exceptions/MissingOptionsException.php
+++ Exceptions/MissingOptionsException.php
@@ -1,1 +1,20 @@
+<?php
 
+/**
+ * Created by PhpStorm.
+ * User: sjhc1170
+ * Date: 20/06/2016
+ * Time: 09:41
+ */
+
+namespace Iriven\Exceptions;
+/**
+ * Exception thrown when a required option is missing.
+ *
+ * Add the option to the passed options array.
+ *
+ * @author Bernhard Schussek <bschussek@gmail.com>
+ */
+class MissingOptionsException extends InvalidArgumentException
+{
+}
