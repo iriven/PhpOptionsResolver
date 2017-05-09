a24ac2c4600af40f99860ab2c2396a294d4ce368
Alfred Tchondjo <iriven@yahoo.fr>
2017-05-09 19:28:31 +0200

    Update UndefinedOptionsException.php

--- Exceptions/UndefinedOptionsException.php
+++ Exceptions/UndefinedOptionsException.php
@@ -1,1 +1,20 @@
+<?php
 
+/**
+ * Created by PhpStorm.
+ * User: sjhc1170
+ * Date: 20/06/2016
+ * Time: 09:41
+ */
+
+namespace Iriven\Exceptions;
+/**
+ * Exception thrown when an undefined option is passed.
+ *
+ * You should remove the options in question from your code or define them
+ * beforehand.
+ *
+ */
+class UndefinedOptionsException extends InvalidArgumentException
+{
+}
