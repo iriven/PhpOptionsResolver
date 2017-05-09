dc1968d00dce17848bd6fbd7a6ae2c374a99492b
Alfred Tchondjo <iriven@yahoo.fr>
2017-05-09 19:27:45 +0200

    Update OptionDefinitionException.php

--- Exceptions/OptionDefinitionException.php
+++ Exceptions/OptionDefinitionException.php
@@ -1,1 +1,19 @@
+<?php
 
+/**
+ * Created by PhpStorm.
+ * User: sjhc1170
+ * Date: 20/06/2016
+ * Time: 09:41
+ */
+
+namespace Iriven\Exceptions;
+use Iriven\Interfaces\ExceptionInterface;
+
+/**
+ * Thrown when two lazy options have a cyclic dependency.
+ *
+ */
+class OptionDefinitionException extends \LogicException implements ExceptionInterface
+{
+}
