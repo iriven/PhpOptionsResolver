250be381dd6dcc1d9e537e9c9adb6003b7058a72
Alfred Tchondjo <iriven@yahoo.fr>
2017-05-09 19:24:08 +0200

    Update InvalidArgumentException.php

--- Exceptions/InvalidArgumentException.php
+++ Exceptions/InvalidArgumentException.php
@@ -1,1 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sjhc1170
+ * Date: 20/06/2016
+ * Time: 09:41
+ */
 
+namespace Iriven\Exceptions;
+use Iriven\Interfaces\ExceptionInterface;
+class InvalidArgumentException extends \InvalidArgumentException implements ExceptionInterface
+{
+}
